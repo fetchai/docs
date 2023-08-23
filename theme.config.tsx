ee3da7b4b8d496552cdb24b79a9cab477908cb45
Shanu Goyanka <shanu.goyanka@greatlearning.in>
2023-08-23 12:15:46 +0530

    removed side nav for landing page and kept header and footers in it

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,15 +3,18 @@ import { DocsThemeConfig } from './theme/fetch-ai-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Fetch.ai Documentation</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
+  // project: {
+  //   link: 'https://github.com/shuding/nextra-docs-template',
+  // },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: 'Fetch.ai 2023',
+  },
+  search: {
+    placeholder: 'Search in documentation'
   }
 }
 
