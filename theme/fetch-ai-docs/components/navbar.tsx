d8c9f026354a167ebe21c9bd72e4a4af55cee1c5
josh <josh@fetch.ai>
2023-08-25 10:58:29 +0100

    header margin

--- theme/fetch-ai-docs/components/navbar.tsx
+++ theme/fetch-ai-docs/components/navbar.tsx
@@ -95,7 +95,7 @@ export function Navbar({ flatDirectories, items }: NavBarProps): ReactElement {
           'contrast-more:nx-shadow-[0_0_0_1px_#000] contrast-more:dark:nx-shadow-[0_0_0_1px_#fff]'
         )}
       />
-      <nav className="nx-mx-auto nx-py-4 nx-max-w-[90rem] nx-items-center nx-justify-end nx-gap-2 nx-pl-[max(env(safe-area-inset-left),1.5rem)] nx-pr-[max(env(safe-area-inset-right),1.5rem)]">
+      <nav className="nx-mx-auto nx-mt-6 nx-py-4 nx-max-w-[90rem] nx-items-center nx-justify-end nx-gap-2 nx-pl-[max(env(safe-area-inset-left),1.5rem)] nx-pr-[max(env(safe-area-inset-right),1.5rem)]">
         <div className="nx-flex">
           {config.logoLink ? (
             <Anchor
