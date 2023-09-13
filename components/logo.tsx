725f5a75842b2929c5d38666dbe8b7a6f6040227
Shanu Goyanka <shanu.goyanka@greatlearning.in>
2023-09-13 13:26:32 +0530

    Added new logo file

---
+++ components/logo.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Image from 'next/image';
+import documentationLogo from '../src/svgs/docs-logo.svg'
+import styles from './landing.module.css'
+
+const Logo: React.FC = () => {
+  return (
+    <Image
+        src={documentationLogo} 
+        alt="Logo"
+        className={styles.docsLogo}
+      />
+   
+  );
+};
+
+export default Logo;
