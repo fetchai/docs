6eb22cc84c6391b7f59867bc7964f985645d5cd0
gautamgambhir97 <gautam.gambhir@fetch.ai>
2023-11-01 16:43:20 +0530

    fix: uncomment the communicating with agent tests

--- agents_tests/test_fetch_agents.py
+++ agents_tests/test_fetch_agents.py
@@ -25,8 +25,8 @@ class TestFetchAgents:
     def test_storage_function(self):
         iterate_over_mdx_files("storage-function")
 
-    # def test_agents(self):
-    #     agent_function("communicating-with-other-agents")
+    def test_agents(self):
+        agent_function("communicating-with-other-agents")
 
     def test_booking_demo(self):
         booking_demo("booking-demo")
