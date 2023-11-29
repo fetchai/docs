1120340740d9236cfd8b287ecf28316216095b75
Shanu Goyanka <goyankashanu@gmail.com>
2023-11-30 00:49:13 +0530

    Added tags in rendered results

--- theme/fetch-ai-docs/components/instant-algolia-search.tsx
+++ theme/fetch-ai-docs/components/instant-algolia-search.tsx
@@ -14,7 +14,12 @@ import { useConfig } from "../contexts";
 import { remark } from "remark";
 import remarkHTML from "remark-html";
 import React from "react";
+import type { Item as NormalItem } from "nextra/normalize-pages";
 
+type MyItem = NormalItem & {
+  // Add or modify properties as needed
+  tags?: string[];
+};
 // Replace with your Algolia credentials
 const searchClient = algoliasearch(
   "13E7QJOAO8",
@@ -27,7 +32,11 @@ const markdownToHTML = (markdownString) => {
   return remark().use(remarkHTML).processSync(markdownString).toString();
 };
 
-export const InstantAlgoliaSearch = () => {
+export const InstantAlgoliaSearch = ({
+  directories,
+}: {
+  directories: MyItem[];
+}) => {
   const config = useConfig();
   // const router = useRouter()
   const [show, setShow] = useState(false);
@@ -105,9 +114,13 @@ export const InstantAlgoliaSearch = () => {
         setShow(true);
       }}
       placeholder="Search..."
-      className="nx-rounded-xxl nx-border-purple nx-border nx-w-full nx-h-full nx-p-4"
+      className="nx-rounded-xxl nx-border-purple nx-border nx-w-full nx-h-full nx-px-4 nx-py-2"
     />
   ));
+
+  const directoriesWithTags = directories
+    .filter((directory) => !!("tags" in directory))
+    .map(({ route, tags }) => ({ route, tags }));
 
   // Custom Hits component to render search results
   const CustomHits = connectHits(({ hits }) => {
@@ -124,14 +137,44 @@ export const InstantAlgoliaSearch = () => {
       groupedHits[route].push(hit);
     }
 
+    const tagColors = [
+      "bg-indigo",
+      "bg-orange",
+      "bg-light-green",
+      "bg-blue-150",
+      "bg-yellow-150",
+      "bg-red-150",
+    ];
+
     return (
       show && (
         <div className="nextra-scrollbar nx-border nx-border-gray-200 nx-bg-white dark:nx-border-neutral-800 dark:nx-bg-neutral-900 nx-absolute nx-top-full nx-z-20 nx-mt-2 nx-overflow-auto nx-overscroll-contain nx-rounded-xl nx-py-2.5 nx-shadow-xl nx-max-h-[min(calc(50vh-11rem-env(safe-area-inset-bottom)),400px)] md:nx-max-h-[min(calc(100vh-5rem-env(safe-area-inset-bottom)),400px)] nx-inset-x-0 ltr:md:nx-left-auto rtl:md:nx-right-auto contrast-more:nx-border contrast-more:nx-border-gray-900 contrast-more:dark:nx-border-gray-50 nx-max-w-full">
           {Object.entries(groupedHits).map(([route, hitsForRoute]) => (
             <div key={route} onClick={() => setShow(false)}>
               <div className="nx-py-2 nx-bg-grey-200 nx-text-sm nx-text-grey-600 nx-font-semibold nx-tracking-loose nx-uppercase">
-                <span className="nx-block nx-px-4 nx-text-sm">
+                <span className="nx-block nx-px-4 nx-text-sm nx-flex nx-gap-4">
                   <div className="nx-my-1">{route.split("/").pop()}</div>
+                  {directoriesWithTags.map((directory) => {
+                    if (route.includes(directory.route)) {
+                      return (
+                        <div key={directory.route}>
+                          <div className="nx-flex nx-flex-wrap nx-gap-2 nx-max-w-50rem">
+                            {directory.tags.slice(0, 3).map((tag, index) => (
+                              <span
+                                key={index}
+                                className={`nx-text-fetch-main nx-text-xs nx-font-normal nx-rounded nx-px-1 nx-py-1 nx-${
+                                  tagColors[index % tagColors.length]
+                                }`}
+                              >
+                                {tag}
+                              </span>
+                            ))}
+                          </div>
+                        </div>
+                      );
+                    }
+                    return null;
+                  })}
                 </span>
               </div>
 
@@ -154,24 +197,27 @@ export const InstantAlgoliaSearch = () => {
                             {hit.title.replaceAll("#", "")}
                           </div>
 
-                          <div className="nx-relative w-full nx-text-sm nx-leading-tight nx-mt-4 nx-xl:mt-2 nx-mb-2">
-                            <MDXProvider
-                              components={getComponents({
-                                isRawLayout: false,
-                                components: config.components,
-                              })}
-                            >
-                              <div
-                                className="nx-flex-grow-1 nx-text-fetch-content nx-font-normal nx-text-fetch-main nx-text-sm"
-                                dangerouslySetInnerHTML={{
-                                  __html: markdownToHTML(hit.content).slice(
-                                    0,
-                                    2000,
-                                  ),
-                                }}
-                              />
-                            </MDXProvider>
-                          </div>
+                          {hit.content.length > 0 && (
+                            <div className="nx-relative w-full nx-text-sm nx-leading-tight nx-mt-4 nx-xl:mt-2 nx-mb-2">
+                              <MDXProvider
+                                components={getComponents({
+                                  isRawLayout: false,
+                                  components: config.components,
+                                })}
+                              >
+                                <div
+                                  className="nx-flex-grow-1 nx-text-fetch-content nx-font-normal nx-text-fetch-main nx-text-sm"
+                                  dangerouslySetInnerHTML={{
+                                    __html: `${markdownToHTML(
+                                      hit.content,
+                                    ).slice(0, 500)}${
+                                      hit.content.length > 500 ? "..." : ""
+                                    }`,
+                                  }}
+                                />
+                              </MDXProvider>
+                            </div>
+                          )}
                           <div className="nx-text-sm nx-text-grey-600">
                             {hit.path
                               .replace(/^\/docs\//, "")
