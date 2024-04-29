131235102244765c56de7ca21975283540eba55e
gautamgambhir97 <140384949+gautamgambhir97@users.noreply.github.com>
2024-04-29 15:09:09 +0530

    feat(misc): updated 404 page design (#556)

---
+++ components/error-404.tsx
@@ -0,0 +1,57 @@
+import React, { ReactNode, useEffect } from "react";
+import Image from "next/image";
+import Error from "../src/svgs/desktop/404.svg";
+import { FeatureGuideTabs } from "./feature-guide-tabs";
+
+const Description = ({ children }: { children: ReactNode }) => (
+  <span
+    style={{
+      color: "rgba(11, 23, 66, 0.8)",
+      fontSize: "18px",
+      fontWeight: "300",
+    }}
+  >
+    {children}
+  </span>
+);
+
+const Error404 = () => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      window.location.href = "/docs";
+    }, 10_000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+  return (
+    <div className="nx-flex nx-flex-col nx-justify-center nx-items-center">
+      <Image alt="error-404" className="nx-w-full nx-h-full" src={Error} />
+      <div className="nx-flex nx-gap-6 nx-flex-col nx-justify-center nx-items-center">
+        <Description>
+          Oops, we can’t find the page you’re looking for.
+        </Description>
+        <div className="nx-flex nx-flex-col nx-justify-center nx-items-center">
+          <Description>
+            You can{" "}
+            <span
+              className="underLineLink"
+              onClick={() => window.location.replace("/docs")}
+            >
+              go back to the homepage or
+            </span>
+          </Description>
+          <Description>
+            The quick links below may help you find your way.
+          </Description>
+        </div>
+      </div>
+      <section className="nx-mt-landing-page-sections nx-max-w-[1216px]">
+        <FeatureGuideTabs centerMode={true} />
+      </section>
+    </div>
+  );
+};
+
+export default Error404;
