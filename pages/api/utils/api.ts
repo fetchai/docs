2d5180b265f3a060f74182df2294fb1bd7d90016
Shanu Goyanka <goyankashanu@gmail.com>
2024-05-10 12:26:41 +0530

    Fixed minor issues

--- pages/api/utils/api.ts
+++ pages/api/utils/api.ts
@@ -1,38 +1,6 @@
 import { setCookie, getCookie } from "cookies-next";
-import jwt from "jsonwebtoken";
-
-class CustomError extends Error {
-  status: number;
-
-  constructor(message: string, status: number) {
-    super(message);
-    this.status = status;
-  }
-}
-
-export const getNewAccessToken = async (refreshToken: string) => {
-  try {
-    const response = await fetch(`https://accounts.fetch.ai/v1/tokens`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        grant_type: "refresh_token",
-        refresh_token: refreshToken,
-      }),
-    });
-    const data = await response.json();
-    if (response.ok && response.status === 200) {
-      return data.access_token;
-    }
-    if (response.status === 422) {
-      throw new CustomError("Refresh token expired", 422);
-    }
-  } catch (refreshError) {
-    throw new CustomError(refreshError.message, 422);
-  }
-};
+import { getNewAccessToken } from "src/core/fauna";
+import { parseJwt } from "src/core/jwt";
 
 export const getAccessToken = async (req, res) => {
   try {
@@ -44,15 +12,15 @@ export const getAccessToken = async (req, res) => {
     }
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const decodedToken: any = jwt.decode(accessToken);
+    const decodedToken: any = parseJwt(accessToken);
     if (!decodedToken) {
       throw { message: "Invalid access token" };
     }
 
-    if (decodedToken.exp * 1000 < Date.now() - 5000) {
+    if (decodedToken.expiry < Date.now() - 5000) {
       // Access token expired, refresh it
       const newAcessToken = await getNewAccessToken(refreshToken);
-      accessToken = newAcessToken;
+      accessToken = newAcessToken.accessToken;
       setCookie("fauna", newAcessToken, { req, res });
     }
 
