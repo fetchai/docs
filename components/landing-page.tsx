376fef2875cb5828f27e0926be0d13fc028e6cc8
Joshua Croft <joshuacroft@Joshuas-MacBook-Pro.local>
2024-08-19 11:14:31 +0100

    updating wording

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -145,8 +145,7 @@ function LandingPage() {
         <p className={styles.welcomeText}>Welcome to</p>
         <h1 className={styles.mainTitle}>The Fetch.ai developer portal</h1>
         <p className={styles.description}>
-          Explore our documentation, guides and examples to get to know Fetch.ai
-          tools and products.
+          Explore our documentation, guides and examples to get to know Fetch.ai tools and products.
         </p>
       </div>
       <section className="nx-mt-60">
