52e95ae395f3b8fc823b1142f671b59736dfdbf2
Joshua Croft <joshuacroft@Joshuas-MacBook-Pro.local>
2024-08-21 16:32:39 +0100

    updating font

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,53 +1,58 @@
 import React from "react";
 // eslint-disable-next-line unicorn/prevent-abbreviations
-import { DocsThemeConfig } from "./theme/fetch-ai-docs";
+import {DocsThemeConfig} from "./theme/fetch-ai-docs";
 import Footer from "./components/footer";
 import Logo from "./components/logo";
 
 const config: DocsThemeConfig = {
-  logo: <Logo />,
-  project: {
-    link: "https://github.com/fetchai",
-  },
-  chat: {
-    link: "https://discord.gg/fetchai",
-  },
-  docsRepositoryBase: "https://github.com/fetchai/docs/tree/master/pages",
-  footer: {
-    component: <Footer />,
-  },
-  search: {
-    placeholder: "Search in documentation",
-  },
-  head: (
-    <>
-      <meta name="msapplication-TileColor" content="#fff" />
-      <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Fetch.ai Documentation" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@fetch_ai" />
-      <meta property="og:title" content="Fetch.ai Documentation" />
-      <meta
-        property="og:description"
-        content="Explore our documentation, guides and examples to get to know Fetch.ai tools and products."
-      />
-      <meta property="og:image" content="/docs/Logo.png" />
-      <meta
-        name="apple-mobile-web-app-title"
-        content="Fetch.ai Documentation"
-      />
-      <link rel="icon" href="/docs/favicon.png" sizes="any" />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    logo: <Logo/>,
+    project: {
+        link: "https://github.com/fetchai",
+    },
+    chat: {
+        link: "https://discord.gg/fetchai",
+    },
+    docsRepositoryBase: "https://github.com/fetchai/docs/tree/master/pages",
+    footer: {
+        component: <Footer/>,
+    },
+    search: {
+        placeholder: "Search in documentation",
+    },
+    head: (
+        <>
+            <meta name="msapplication-TileColor" content="#fff"/>
+            <meta httpEquiv="Content-Language" content="en"/>
+            <meta name="description" content="Fetch.ai Documentation"/>
+            <meta name="twitter:card" content="summary_large_image"/>
+            <meta name="twitter:site" content="@fetch_ai"/>
+            <meta property="og:title" content="Fetch.ai Documentation"/>
+            <meta
+                property="og:description"
+                content="Explore our documentation, guides and examples to get to know Fetch.ai tools and products."
+            />
+            <meta property="og:image" content="/docs/Logo.png"/>
+            <meta
+                name="apple-mobile-web-app-title"
+                content="Fetch.ai Documentation"
+            />
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link
+                href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+                rel="stylesheet"/>
+            <link rel="icon" href="/docs/favicon.png" sizes="any"/>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-MVT793SR');`,
-        }}
-      />
-    </>
-  ),
+                }}
+            />
+        </>
+    ),
 };
 
 export default config;
