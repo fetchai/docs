164a79f08b27d58bddf1e51ce81ea7ffd4376c92
Joshua Croft <joshuacroft@Joshuas-MacBook-Pro.local>
2024-09-19 15:26:49 +0100

    changes from code review

--- check_spelling.py
+++ check_spelling.py
@@ -19,6 +19,7 @@ import_pattern = re.compile(r'import\s*{\s*([\s\S]*?)\s*}\s*from\s*["\']([^"\']+
 jsx_like_tags_pattern = re.compile(r'<[^>]*>[\s\S]*?<\/[^>]*>|<[^>]*?/>', re.DOTALL)
 path_pattern = re.compile(r'path:\s*"/[^"]*"')
 guidebox_pattern = re.compile(r'<GuideBox[\s\S]*?/>', re.IGNORECASE)
+hex_colours = re.compile(r'([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})')
 
 # Function to extract text while ignoring specified components and handling code blocks differently
 def extract_text_from_mdx(file_path):
@@ -65,6 +66,7 @@ def extract_text_from_mdx(file_path):
 
 # Function to check for spelling errors
 def check_spelling(text, is_code_block=False):
+
     def split_underscore_words(word):
         return re.split(r'[_\s]+', word)
 
@@ -89,8 +91,9 @@ def check_directory(directory):
     for root, _, files in os.walk(directory):
         for file in files:
             if file.endswith('.mdx'):
+
                 file_path = os.path.join(root, file)
-                print(f'Checking file: {file_path}')
+                print(f'========== Checking file: {file_path} ==========')
 
                 # Extract text and code blocks from the MDX file
                 text, code_blocks = extract_text_from_mdx(file_path)
@@ -103,16 +106,21 @@ def check_directory(directory):
                         print(f'  - {error}')
                     has_errors = True
                 else:
-                    print(f'No spelling errors found in the main text of {file_path}')
+                    pass
+                    # print(f'No spelling errors found in the main text of {file_path}')
 
                 # Check for spelling errors in code blocks (warnings)
+                warnings = []
                 for code_block in code_blocks:
                     warnings = check_spelling(code_block, is_code_block=True).get('warnings', [])
-                    if warnings:
+
+                if warnings:
                         print(f'Warnings (spelling errors in code block) in {file_path}:')
                         for warning in warnings:
                             print(f'  - {warning}')
-                        has_errors = True
+
+
+
 
     return has_errors
 
