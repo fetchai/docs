4639664fdc27b0bd1d0b7d6f8ebb0868349ffc69
gautamgambhir97 <gautam.gambhir@fetch.ai>
2025-01-03 16:59:56 +0530

    feat: update the design for chat with us

--- components/chat/chat-with-us.tsx
+++ components/chat/chat-with-us.tsx
@@ -1,33 +1,23 @@
-'use client'
-import React, { useState } from 'react'
-import ChatWindow from './chat-window'
-// import Button from '../button/new-button'
-import Image from 'next/image'
+"use client";
+import React, { useState } from "react";
+import ChatWindow from "./chat-window";
+import { AgentIcon } from "src/icons/shared-icons";
 
 const ChatWithUs = () => {
-    const [isChatOpen, setIsChatOpen] = useState(false)
+  const [isChatOpen, setIsChatOpen] = useState(false);
 
-    return (
-        <>
-            {/* Floating Button */}
-            <button
-                onClick={() => setIsChatOpen(!isChatOpen)}
-                className="nx-fixed nx-bottom-12 nx-right-12 !nx-px-6 !nx-py-4 nx-z-40 nx-flex"
-            >
-                <Image
-                    src="/Images/icons/agent-icon.svg"
-                    alt="agentverse-img"
-                    width={23}
-                    height={24}
-                    className="nx-w-6 nx-h-6 nx-my-auto"
-                />
-                Chat with us
-            </button>
+  return (
+    <>
+      <button
+        onClick={() => setIsChatOpen(!isChatOpen)}
+        className="nx-fixed nx-bottom-12 nx-right-12 !nx-px-6 !nx-py-4 nx-z-40 nx-flex nx-bg-chatwithus nx-text-white nx-rounded-lg nx-font-medium nx-gap-4"
+      >
+        <AgentIcon />
+        Chat with us
+      </button>
+      {isChatOpen && <ChatWindow onClose={() => setIsChatOpen(false)} />}
+    </>
+  );
+};
 
-            {/* Chat Window */}
-            {isChatOpen && <ChatWindow onClose={() => setIsChatOpen(false)} />}
-        </>
-    )
-}
-
-export default ChatWithUs
+export default ChatWithUs;
--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,32 +1,32 @@
 // src/pages/api/chat.ts
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse,
 ) {
-    if (req.method === 'POST') {
-        const { message } = req.body
+  if (req.method === "POST") {
+    const { message } = req.body;
 
-        try {
-            const response = await fetch(
-                'https://chat-with-docs-rzql.onrender.com/echo',
-                {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({ message: message }),
-                }
-            )
-            const data = await response.json()
-            return res.status(200).json({ reply: data.message })
-        } catch (error) {
-            console.log('error', error)
-            return res
-                .status(500)
-                .json({ reply: 'Something went wrong. Please try again.' })
-        }
-    } else {
-        res.setHeader('Allow', ['POST'])
-        res.status(405).end(`Method ${req.method} Not Allowed`)
+    try {
+      const response = await fetch(
+        "https://chat-with-docs-rzql.onrender.com/echo",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ message: message }),
+        },
+      );
+      const data = await response.json();
+      return res.status(200).json({ reply: data.message });
+    } catch (error) {
+      console.log("error", error);
+      return res
+        .status(500)
+        .json({ reply: "Something went wrong. Please try again." });
     }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
