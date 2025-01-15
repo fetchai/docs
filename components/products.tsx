bfd613438d72f516b3f629944d1349720f9e8683
gautamgambhir97 <gautam.gambhir@fetch.ai>
2025-01-15 18:59:01 +0530

    fix font issue and remove console

--- components/products.tsx
+++ components/products.tsx
@@ -71,7 +71,7 @@ const Item = ({
             className={
               hover
                 ? "nx-text-purple nx-text-base nx-font-medium nx-mb-2"
-                : "nx-text-black dark:nx-text-[#E9E9EA] nx-text-lg nx-font-medium nx-mb-2"
+                : "nx-text-black dark:nx-text-[#E9E9EA] nx-text-base nx-font-medium nx-mb-2"
             }
           >
             {item.title}
