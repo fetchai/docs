067bccf0c0be9cea1f970134728b0c7a12d25745
Shanu Goyanka <goyankashanu@gmail.com>
2025-06-02 12:19:17 +0530

    fix(html): Fixed typo and added back chat with us option

--- components/chat/chat-with-us.tsx
+++ components/chat/chat-with-us.tsx
@@ -1,7 +1,8 @@
 "use client";
 import React, { useState } from "react";
 import ChatWindow from "./chat-window";
-import { AgentIcon } from "src/icons/shared-icons";
+import Image from "next/image";
+import FetchWhite from "src/images/fetch_logo_only_white.svg";
 
 const ChatWithUs = () => {
   const [isChatOpen, setIsChatOpen] = useState(false);
@@ -10,9 +11,15 @@ const ChatWithUs = () => {
     <>
       <button
         onClick={() => setIsChatOpen(!isChatOpen)}
-        className="nx-fixed nx-bottom-12 nx-right-12 !nx-px-6 !nx-py-4 nx-z-40 nx-flex nx-bg-chatwithus nx-text-white nx-rounded-lg nx-font-medium nx-gap-4"
+        className="nx-fixed nx-bottom-12 nx-right-12 !nx-px-6 !nx-py-4 nx-z-40 nx-flex nx-bg-chatwithus nx-uppercase nx-font-medium geist-mono-font-family nx-text-white nx-rounded-lg nx-gap-2"
       >
-        <AgentIcon />
+        <Image
+          src={FetchWhite}
+          alt="agentverse-img"
+          width={16}
+          height={16}
+          className="nx-w-[16px] nx-h-[16px] nx-my-auto"
+        />
         Chat with us
       </button>
       {isChatOpen && <ChatWindow onClose={() => setIsChatOpen(false)} />}
